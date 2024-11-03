c73805b2ba967ed79ab8fcb8e2141c28ceebbed1
jannick <jannick.schubert@student.kit.edu>
2024-11-03 19:08:50 +0100

    Normalenberechnung hinzugefügt

--- PointCloudBunny.py
+++ PointCloudBunny.py
@@ -13,3 +13,5 @@ pcd.normals = o3d.utility.Vector3dVector(np.zeros(
     (1, 3)))  # invalidate existing normals
 pcd.estimate_normals()
 o3d.visualization.draw_geometries([pcd], point_show_normal=True)
+
+print("test")
